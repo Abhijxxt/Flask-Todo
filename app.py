8ff27552db68c0c06f71546625a47aec3fc1828d
Abhijeet Chatterjee <97887115+Abhijxxt@users.noreply.github.com>
2024-09-07 00:03:25 +0530

    Database created, Add, Delete action created
    using flask shell we create the database and then add and delete methods are created

--- app.py
+++ app.py
@@ -3,17 +3,42 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todolist.db'
+
 db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
+
+class Todo(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    message = db.Column(db.String(length=200), nullable=False)
 
 @app.route('/', methods=['POST','GET'])
 @app.route('/home', methods=['POST','GET'])
 def HomePage():
     if request.method == 'GET':
-        return render_template('home.html')
+        todo_data = Todo.query.all()
+        return render_template('home.html', todos=todo_data)
     else:
         todo_message = request.form['todo']
-        print(todo_message)
+        todo_model = Todo(message=todo_message)
+        try:
+            db.session.add(todo_model)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error Occured"
+
+@app.route('/delete/<int:id>')
+def deletePage(id):
+    try:
+        delete_todo = Todo.query.get_or_404(id)
+        db.session.delete(delete_todo)
+        db.session.commit()
         return redirect('/')
+    except:
+        return "There was an error deleting message"
 
 if __name__ == '__main__':
     app.run(debug=True)
