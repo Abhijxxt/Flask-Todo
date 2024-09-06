8bfb4d4ad7e3ba7e6011748dd99508bfeb781737
Abhijeet Chatterjee <97887115+Abhijxxt@users.noreply.github.com>
2024-09-06 23:41:13 +0530

    Database initiated Form added

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['POST','GET'])
 @app.route('/home', methods=['POST','GET'])
 def HomePage():
-    return render_template('home.html')
-
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        todo_message = request.form['todo']
+        print(todo_message)
+        return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
